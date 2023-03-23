742c1d74303e71a8d6de7ddce011bed0a781bd1f
preciousimo <preciousimoniakemu@gmail.com>
2023-03-23 23:35:31 +0100

    Register models in admin

--- trading/admin.py
+++ trading/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import*
 
-# Register your models here.
+admin.site.register(TradingAccount)
+admin.site.register(Equity)
+admin.site.register(Balance)
+admin.site.register(MarketWatchTime)
